6ff9bf69be205c2771e701e0fa14b36c949f98ad
Neel-13-ai <pneel2948@gmail.com>
2025-12-06 17:10:57 +0530

    change path of app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ import AddBug from "./pages/Bug/AddBug";
 import EditBug from "./pages/Bug/UpdateBug";
 import AdminDashboard from "./components/dashBoardHome";
 import Login from "./pages/Login";
-
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated } = useAuth();
   if (!isAuthenticated) {
