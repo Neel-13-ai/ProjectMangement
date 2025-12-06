c2c63c81fce4b8a4ada5d8f1cc6d64807fc7d9b2
Neel-13-ai <pneel2948@gmail.com>
2025-12-06 19:12:42 +0530

    file extenstion added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useAuth } from "./context/authContext";
 import { Toaster } from "react-hot-toast";
 
 import UserProfile from "./pages/UserProfile";
-import Login from "./pages/Login";
+import Login from "./pages/Login.jsx";
 import DashboardLayout from "./components/dashboard";
 import UserList from "./pages/users/UserList";
 import AddUser from "./pages/users/AddUser";
